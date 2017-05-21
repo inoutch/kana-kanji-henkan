f8ec0c1cd0455432a8e2eb2b7465e6bf0cdb27de
Inoue Akinori <al13011@shibaura-it.ac.jp>
2017-05-21 17:38:43 +0900

    Clean source

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kana-kanji-henkan
 Convert kana to kanji
+
+## How to run
+Windows
+`gradlew.bat run`
+
+Linux
+`./gradlew run`
--- src/main/java/KanaHenkan.java
+++ src/main/java/KanaHenkan.java
@@ -2,17 +2,26 @@ import analyzer.CorpusCostManager;
 import analyzer.SyntaxAnalysisResult;
 import analyzer.SyntaxAnalyzer;
 
+import java.util.Scanner;
+
 public class KanaHenkan {
+
     public static void main(String args[]) {
-        String testText = "こんにちは、あなたはなにものですか";
-
         try {
-            CorpusCostManager corpusCostManager = new CorpusCostManager("tangoCost_ver2_s_utf8.csv");
-            SyntaxAnalyzer analyzer = new SyntaxAnalyzer(corpusCostManager);
-            SyntaxAnalysisResult result = analyzer.convert(testText);
+            System.out.print("Please enter kana text: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
 
-            System.out.println("入力: " + testText);
-            System.out.println("出力: " + result.toString());
+            System.out.println("--- Loading corpus ---");
+            CorpusCostManager corpusCostManager = new CorpusCostManager("tangoCost_ver2_s_utf8.csv");
+
+
+            SyntaxAnalyzer analyzer = new SyntaxAnalyzer(corpusCostManager);
+            SyntaxAnalysisResult result = analyzer.convert(input);
+
+            System.out.println("--- Loading corpus ---");
+            System.out.println("Input\t: " + input);
+            System.out.println("Output\t: " + result.toString());
 
         } catch (Exception e) {
             e.printStackTrace();
